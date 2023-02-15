0c74122540ae2106b6a3f1885faebf3b1fcb50cf
qkral <61017598+qkral@users.noreply.github.com>
2023-02-15 13:20:14 +0100

    first deploy

--- arxivsummarizer/settings.py
+++ arxivsummarizer/settings.py
@@ -35,7 +35,7 @@ EMAIL_USE_TLS = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['summarizepaper.herokuapp.com']
 
 
 # Application definition
