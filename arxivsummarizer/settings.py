90568ebdef4a587818d75a5da5df7d832a35c052
qkral <61017598+qkral@users.noreply.github.com>
2023-02-15 11:58:52 +0100

    first deploy

--- arxivsummarizer/settings.py
+++ arxivsummarizer/settings.py
@@ -154,6 +154,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 '''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
