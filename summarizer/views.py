5efeaa0708315e5998c3031bf8ba7dcccf7fda62
qkral <61017598+qkral@users.noreply.github.com>
2023-03-02 21:33:54 +0100

    change to fpdf2

--- summarizer/views.py
+++ summarizer/views.py
@@ -524,7 +524,7 @@ def arxividpage(request, arxiv_id, error_message=None, cat=None):
 
             response = HttpResponse(bytes(pdf_bytes).encode('latin-1'), content_type='application/pdf')
 
-            response = HttpResponse(content_type="application/pdf")
+            #response = HttpResponse(content_type="application/pdf")
             filename="SummarizePaper-"+str(arxiv_id)+".pdf"
             response['Content-Disposition'] = 'attachment; filename=%s' % filename  # force browser to download file
             #response.write(resp)
