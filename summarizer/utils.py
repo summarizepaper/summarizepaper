590c084b1deb0cc7bc32276279cd3e37f35098da
qkral <61017598+qkral@users.noreply.github.com>
2023-03-02 22:23:52 +0100

    change to fpdf2

--- summarizer/utils.py
+++ summarizer/utils.py
@@ -560,9 +560,9 @@ def summary_pdf(arxiv_id,language):
 
             def sectionhtml(self, title, html):
                 #self.set_font("DejaVu", "B", size=12)
-                self.set_font("Arial","B", size=12)
+                self.set_font("DejaVu","B", size=12)
                 self.cell(0, 10, title, 0, 1)
-                self.set_font("Arial", size=11)
+                self.set_font("DejaVu", size=11)
                 texthtml="<font color='#000000'>"+html+"</font>"
                 print('te',texthtml)
                 self.write_html(texthtml)
