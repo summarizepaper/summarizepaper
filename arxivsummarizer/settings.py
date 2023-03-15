66513871fd8ba23e6f7bc64933e9abc585e90f58
qkral <61017598+qkral@users.noreply.github.com>
2023-03-15 20:42:21 +0100

    limit the number of papers per 10s or per day

--- arxivsummarizer/settings.py
+++ arxivsummarizer/settings.py
@@ -42,7 +42,7 @@ if 'ON_HEROKU' in os.environ:
 else:
     DEBUG = True
 
-CSRF_TRUSTED_ORIGINS = ['https://summarizepaper.herokuapp.com','https://summarizepaper.com/','https://www.summarizepaper.com/','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://summarizepaper.herokuapp.com','https://summarizepaper.com','https://www.summarizepaper.com','https://*.127.0.0.1']
 
 ALLOWED_HOSTS = ['summarizepaper.herokuapp.com','summarizepaper.com','127.0.0.1']
 
